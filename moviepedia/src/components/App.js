4267ed60c846f1c73c993f26d9a5fb30f2ba0e06
seung-heee <tmdgmldi2595@naver.com>
2023-03-04 20:40:08 +0900

    moviepedia

---
+++ moviepedia/src/components/App.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import ReviewList from "./ReviewList";
+import { getReviews } from "../api";
+
+const LIMIT = 6;
+
+function App() {
+  const [order, setOrder] = useState("createdAt");
+  const [items, setItems] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
+  const handleLoad = async (options) => {
+    let result;
+    try {
+      setIsLoading(true);
+      result = await getReviews(options);
+    } catch (error) {
+      console.error(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+
+    const { reviews, paging } = await getReviews(options);
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems((prevItems) => [...prevItems, ...reviews]);
+    }
+    setOffset(options.offset + reviews.length);
+    setHasNext(paging.hasNext);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
+  };
+
+  useEffect(() => {
+    handleLoad({ order, offset: 0, limit: LIMIT });
+  }, [order]);
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더 보기
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default App;
