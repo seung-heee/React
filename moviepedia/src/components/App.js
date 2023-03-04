4ad4df13e1aafbe8abd46bc7e01219c8de67d90e
seung-heee <tmdgmldi2595@naver.com>
2023-03-04 21:01:31 +0900

    all

--- moviepedia/src/components/App.js
+++ moviepedia/src/components/App.js
@@ -56,6 +56,8 @@ function App() {
       <div>
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleBestClick}>베스트순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+        <button onClick={handleBestClick}>베스트순</button>
       </div>
 
       <ReviewList items={sortedItems} onDelete={handleDelete} />
