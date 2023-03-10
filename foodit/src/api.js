a0526a78661244df654cc2e2693d6b26307eb528
seung-heee <tmdgmldi2595@naver.com>
2023-03-10 19:29:45 +0900

    pagenation cursor

---
+++ foodit/src/api.js
@@ -0,0 +1,6 @@
+export async function getFoods({ order = "createdAt", cursor, limit = 10 }) {
+  const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
+  const response = await fetch(`https://learn.codeit.kr/2595/foods?${query}`); // fetch로 리퀘스트 후 리스폰스를 받고
+  const body = await response.json(); // 리스폰스 객체의 json 메소드를 사용해서 바디
+  return body; // 리턴
+}
--- foodit/src/components/App.js
+++ foodit/src/components/App.js
@@ -1,10 +1,11 @@
 import FoodList from "./FoodList";
-import { useState } from "react";
-import mockItems from "../mock.json";
+import { useEffect, useState } from "react";
+import { getFoods } from "../api";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
+  const [cursor, setCursor] = useState(null); // 커서값 저장할 함수
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]); // 정렬
 
@@ -17,6 +18,30 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoad = async (options) => {
+    const {
+      foods,
+      paging: { nextCursor },
+    } = await getFoods(options);
+    if (!options.cursor) {
+      setItems(foods);
+    } else {
+      setItems((prevItems) => [...prevItems, ...foods]);
+    }
+    setCursor(nextCursor);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({
+      order,
+      cursor,
+    });
+  };
+
+  useEffect(() => {
+    handleLoad(order); // 콜백 함수
+  }, [order]);
+
   return (
     <div>
       <div>
@@ -24,6 +49,8 @@ function App() {
         <button onClick={handleCalorieClick}>칼로리순</button>
       </div>
       <FoodList items={sortedItems} onDelete={handleDelete} />
+      {/* cursor가 false면 실행 안됨. 조건부 렌더링 */}
+      {cursor && <button onClick={handleLoadMore}>더보기</button>}
     </div>
   );
 }
