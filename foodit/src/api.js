86b7dccf577a4f5160fb8076868ce08e40173e2f
seung-heee <tmdgmldi2595@naver.com>
2023-03-10 19:33:15 +0900

    cursor undefined...

--- foodit/src/api.js
+++ foodit/src/api.js
@@ -1,5 +1,5 @@
 export async function getFoods({ order = "createdAt", cursor, limit = 10 }) {
-  // 이부분 cursor undefined 뜸ㅠ 해결 못함 아직 app.js!!!!!
+  // 이부분 cursor undefined 뜸,,, 어디서 받아와야하니
   const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
   const response = await fetch(`https://learn.codeit.kr/2595/foods?${query}`); // fetch로 리퀘스트 후 리스폰스를 받고
   const body = await response.json(); // 리스폰스 객체의 json 메소드를 사용해서 바디
