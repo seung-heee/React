7d68b87d269b8b47fd387f9d27311b99ef17d62c
seung-heee <tmdgmldi2595@naver.com>
2023-03-20 22:36:46 +0900

    ReviewList

---
+++ movie/src/components/ReviewList.js
@@ -0,0 +1,40 @@
+import "./ReviewList.css";
+
+// 숫자형식인 createdAt을 날짜 형식으로 변경
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
+// 각 영화 1개 정보
+function ReviewListItem({ item }) {
+  return (
+    <div className="ReviewListItem">
+      <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
+      <div>
+        <h1>{item.title}</h1>
+        <p>{item.rating}</p>
+        <p>{formatDate(item.createdAt)}</p>
+        <p>{item.content}</p>
+      </div>
+    </div>
+  );
+}
+
+// 영화 list
+function ReviewList({ items }) {
+  return (
+    // 각 요소를 map으로 렌더링
+    <ul>
+      {items.map((item) => {
+        return (
+          <li>
+            <ReviewListItem item={item} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default ReviewList;
