6df9138f9f83f2884e95358ba53ff0b3da894416
seung-heee <tmdgmldi2595@naver.com>
2023-03-20 23:15:48 +0900

    map/sort/filter

--- movie/src/components/App.js
+++ movie/src/components/App.js
@@ -1,10 +1,29 @@
 import ReviewList from "./ReviewList";
-import items from "../mock.json";
+import mockitems from "../mock.json";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState(mockitems);
+  const [order, setorder] = useState("createdAt"); // 정렬 state
+  const sortedItems = items.sort((a, b) => b[order] - a[order]); // order에 따라 정렬기준 달라짐
+
+  const handleNewestClick = () => setorder("createdAt"); // 최신순
+  const handleBestClick = () => setorder("rating"); // 평점순
+
+  const handleDelete = (id) => {
+    // 삭제기능, 해당 요소의 id를 가지고 filter를 거쳐 삭제됨.
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
   return (
     <div>
-      <ReviewList items={items} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
--- movie/src/components/ReviewList.js
+++ movie/src/components/ReviewList.js
@@ -1,13 +1,15 @@
 import "./ReviewList.css";
 
-// 숫자형식인 createdAt을 날짜 형식으로 변경
+// 숫자 형식인 createdAt을 날짜 형식으로 변경
 function formatDate(value) {
   const date = new Date(value);
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
 
 // 각 영화 1개 정보
-function ReviewListItem({ item }) {
+function ReviewListItem({ item, onDelete }) {
+  const handleDeleteClick = () => onDelete(item.id);
+
   return (
     <div className="ReviewListItem">
       <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
@@ -16,20 +18,23 @@ function ReviewListItem({ item }) {
         <p>{item.rating}</p>
         <p>{formatDate(item.createdAt)}</p>
         <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제</button>
       </div>
     </div>
   );
 }
 
 // 영화 list
-function ReviewList({ items }) {
+function ReviewList({ items, onDelete }) {
   return (
     // 각 요소를 map으로 렌더링
     <ul>
       {items.map((item) => {
         return (
-          <li>
-            <ReviewListItem item={item} />
+          // 고유한 key 지정
+          <li key={item.id}>
+            <ReviewListItem item={item} onDelete={onDelete} />
+            <input value={item.title}></input>
           </li>
         );
       })}
